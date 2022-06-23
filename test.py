31ded7953bf3ebb8a4944f69851000bf0821b8fb
arnab28122000 <arnab.bhattacharya28122000@gmail.com>
2022-06-24 02:18:35 +0530

    api service

---
+++ test.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'arnab', 'salav')
+insert_query = "INSERT INTO users VALUES(?, ?, ?)"
+cursor.execute(insert_query, user)
+users = [
+    (2, 'aman', 'dfdsfs'),
+    (3, 'sham', 'dfasd')
+]
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+connection.close()
